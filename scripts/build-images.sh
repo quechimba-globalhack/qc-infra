e105a06b6b90db1546552d557369543c78a03fe2
jlriiot <ing.jmontana@gmail.com>
2020-05-30 10:45:30 -0500

    Skaffold configuration for all services

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build -t qc-api:latest ../qc-api/
-# docker build -t qc-contracts:latest ../qc-contracts/
-# docker build -f ../qc-frontend/Dockerfile -t qc-api:latest
+docker build -t qc-contracts:latest ../qc-contracts/
+docker build -t qc-frontend:latest ../qc-frontend/
---
+++ scripts/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+skaffold dev --port-forward
